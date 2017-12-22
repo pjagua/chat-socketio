65f00b22c43246006111f2dd78235f899b40631f
Pedro J. Aguayo <pedro@aguayo.org>
2017-12-22 16:51:07 -0500

    Adding adding logger documentation

--- ChatApp/chatapp/logger.py
+++ ChatApp/chatapp/logger.py
@@ -21,7 +21,7 @@ import logging.handlers
 logformat = '%(asctime)s - %(name)s - %(levelname)s: %(message)s'
 dateformat = '%b %d %Y %I:%M:%S'
 
-class Logger(objecct):
+class Logger(object):
     def __init__(self, logfile, level, loggingfmt=logformat, datefmt=dateformat):
         ''' Setup Logging::
 
@@ -41,19 +41,49 @@ class Logger(objecct):
 
 
     def write(self, buf):
+        ''' Creates a write() for interfacing with open construct 
+
+        param buf: buffer object
+        
+        '''
+
         for line in buf.rstrip().splitlines():
             self.log_handle().log(self.log_level(), linerstrip())
 
     def get_logger(self):
+        ''' Return module name for logging origination '''
+
         return __name__.split(".")[0]
 
     def log_handle(self):
+        ''' Set log handle 
+        
+        return: logger | raise Exception
+
+        '''
+
         logger = logging.getLogger(self.__logger)
         f_handler = logging.handler.TimedRotatingFileHandler(self.__logfile, when='midnight', backupCount=5)
         c_handler = logging.StreamHandler()
         formatter = logging.Formatter(self.__loggingfmt, self.__datefmt)
 
+        try:
+            logger.setLevel(self.log_level())
+            f_handler.setLEvel(self.log_level())
+            logger.addHandler(self.console_handler(logger, formatter))
+        except Exception as e:
+            raise Exception("Failed to set logging handle: {0}".format(e))
+        else:
+            logger.addHandler(f_handler)
+            return logger
+
     def log_level(self):
+        ''' Returns logging levels
+        
+        return: logging.<LEVEL>
+    
+        '''
+
         if self.__level == "DEBUG":
             return logging.DEBUG
         elif self.__level == "CRITICAL":
@@ -68,8 +98,19 @@ class Logger(objecct):
             return logging.NOTSET
 
     def console_hndler(self, logger, h_format):
+        ''' Return the console handler
+
+        param logger: logger object
+        param h_format: Formatter object
+        return ch: Console loghandler
+
+        '''
         if self.__level == 'DEBUG':
-            ch  = logging.StreamHandler()
-            ch.setLevel(logging.DEBUG)
-            ch.setFormatter(h_format)
-            logger.addHandler(ch)
+            try:
+                ch  = logging.StreamHandler()
+                ch.setLevel(logging.DEBUG)
+                ch.setFormatter(h_format)
+            except Exception as e:
+                raise Exception("Unable to set the Console logger handler")
+            else:
+                return ch
